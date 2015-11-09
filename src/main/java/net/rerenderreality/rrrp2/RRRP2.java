986b07ee334bb9dcc436af51e08b0f08dca40623
EliteByte <mario.figueroa87@gmail.com>
2015-11-08 17:52:39 -0800

    EliteBytes Automated Commit

--- src/main/java/net/rerenderreality/rrrp2/RRRP2.java
+++ src/main/java/net/rerenderreality/rrrp2/RRRP2.java
@@ -2,18 +2,7 @@ package net.rerenderreality.rrrp2;
 
 public class RRRP2 {
 	public static void main(String[] args) {
-		for (int i = 0; i > 100; i++)
-			System.out.println("Fuck the world!");
+
 	}
-	
-	public static int fuckinFunction1(int f)
-	{
-		System.out.println("F YOU!!");
-	}
-	
-	public static int fuckinFuntion666(boolean f666)
-	{
-		System.out.println("666");
-	}
-	
+
 }
