217685701c62627e560711df552f3caebee69f10
EliteByte <mario.figueroa87@gmail.com>
2015-11-08 17:49:12 -0800

    EliteBytes Automated Commit

--- src/main/java/net/rerenderreality/rrrp2/RRRP2.java
+++ src/main/java/net/rerenderreality/rrrp2/RRRP2.java
@@ -1,8 +1,23 @@
 package net.rerenderreality.rrrp2;
 
+import java.io.IOException;
+
 public class RRRP2 {
 	public static void main(String[] args) {
 		for (int i = 0; i > 100; i++)
 			System.out.println("Fuck the world!");
 	}
+
+	public void commencePoopanator() {
+
+	}
+
+	public void TWOcool4u() {
+		try {
+			int input = System.in.read();
+		} catch (IOException e) {
+			System.out.println();
+		}
+	}
+
 }
