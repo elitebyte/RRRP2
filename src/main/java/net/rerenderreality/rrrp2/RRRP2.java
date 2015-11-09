8754067a68fc1e20786ef02ec4fd222739daa53f
Avarai <nedac7@gmail.com>
2015-11-08 19:49:03 -0600

    Avarai's Automated Commit Service Providing Excellent Commits Since 2015

--- src/main/java/net/rerenderreality/rrrp2/RRRP2.java
+++ src/main/java/net/rerenderreality/rrrp2/RRRP2.java
@@ -5,4 +5,15 @@ public class RRRP2 {
 		for (int i = 0; i > 100; i++)
 			System.out.println("Fuck the world!");
 	}
+	
+	public static int fuckinFunction1(int f)
+	{
+		System.out.println("F YOU!!");
+	}
+	
+	public static int fuckinFuntion666(boolean f666)
+	{
+		System.out.println("666");
+	}
+	
 }
