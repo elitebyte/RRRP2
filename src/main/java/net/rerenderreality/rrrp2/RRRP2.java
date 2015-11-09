3d2a78c400e8f04d71766e25319972a908a4955a
EliteByte <mario.figueroa87@gmail.com>
2015-11-08 18:04:48 -0800

    EliteBytes Automated Commit

--- src/main/java/net/rerenderreality/rrrp2/RRRP2.java
+++ src/main/java/net/rerenderreality/rrrp2/RRRP2.java
@@ -1,8 +1,12 @@
 package net.rerenderreality.rrrp2;
 
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "example", name = "Example Project", version = "1.0")
 public class RRRP2 {
-	public static void main(String[] args) {
-
+	@Listener
+	public void onServerStart(GameStartedServerEvent event) {
 	}
-
 }
