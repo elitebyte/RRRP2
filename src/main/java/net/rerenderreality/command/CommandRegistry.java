b617591aed7a06b432f1896baf278bf8764ccab8
Avarai <nedac7@gmail.com>
2015-11-12 21:10:12 -0600

    Avarai's Automated Commit

--- src/main/java/net/rerenderreality/command/CommandRegistry.java
+++ src/main/java/net/rerenderreality/command/CommandRegistry.java
@@ -6,10 +6,7 @@ import net.rerenderreality.main.RRRP2;
 
 import org.spongepowered.api.text.Texts;
 import org.spongepowered.api.util.command.spec.CommandSpec;
-import org.spongepowered.api.util.command.dispatcher.*;
-import org.spongepowered.api.*;
 import org.spongepowered.api.plugin.Plugin;
-import org.spongepowered.api.service.command.*;
 
 public class CommandRegistry
 {
